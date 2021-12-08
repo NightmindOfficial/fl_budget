88c63bb5df4b2e9dd4efe930ad4e07f0d8cade0d
NightmindOfficial <mail@otismohr.de>
2021-12-09 00:49:12 +0100

    Video 70

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fl_budget/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,6 +14,35 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+// Liste von Transaktionen
+
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Neue Schuhe',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Latte',
+      amount: 4.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Netflix Subscription',
+      amount: 12.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Otis\' OnlyFans Subscription',
+      amount: 4.20,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
