3d4d3201a0196c2c096c59724b7c15d64bc56207
NightmindOfficial <mail@otismohr.de>
2021-12-21 21:36:24 +0100

    Video 89

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:fl_budget/widgets/user_transactions.dart';
+import 'package:fl_budget/widgets/new_transaction.dart';
+import 'package:fl_budget/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
+
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +17,65 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'Neue Schuhe',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Latte',
+      amount: 4.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Netflix Subscription',
+      amount: 12.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Otis\' OnlyFans Subscription',
+      amount: 4.20,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransactionToList(
+      String transactionTitle, double transactionAmount) {
+    final tBATransaction = Transaction(
+      title: transactionTitle,
+      amount: transactionAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(tBATransaction);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            child: NewTransaction(_addNewTransactionToList),
+            behavior: HitTestBehavior.opaque,
+            onTap: () {},
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +88,7 @@ class MyHomePage extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(context),
             icon: Icon(Icons.add),
           ),
         ],
@@ -45,13 +106,13 @@ class MyHomePage extends StatelessWidget {
                 elevation: 3,
               ),
             ),
-            UserTransactions(),
+            TransactionList(_userTransactions),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => _startAddNewTransaction(context),
         child: Icon(Icons.add),
         backgroundColor: Colors.lightGreen,
       ),
