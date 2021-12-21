52ad0b5b48a1bf5052aae2b2b47b8c586fcfd846
NightmindOfficial <mail@otismohr.de>
2021-12-21 20:59:34 +0100

    Video 87

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,6 +7,20 @@ class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
+  void addTransactionCallFct() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTransactionToList(
+      titleController.text,
+      double.parse(amountController.text),
+    );
+  }
+
   final Function addTransactionToList;
   NewTransaction(this.addTransactionToList, {Key? key}) : super(key: key);
 
@@ -21,19 +35,20 @@ class NewTransaction extends StatelessWidget {
             TextField(
               // onChanged: (value) => titleInput = value,
               controller: titleController,
+              keyboardType: TextInputType.text,
               decoration: InputDecoration(labelText: 'Title'),
+              onSubmitted: (_) => addTransactionCallFct(),
             ),
             TextField(
               // onChanged: (value) => amountInput = value,
               controller: amountController,
+              keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: 'Amount'),
+              onSubmitted: (_) => addTransactionCallFct(),
             ),
             ElevatedButton(
               onPressed: () {
-                addTransactionToList(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
+                addTransactionCallFct();
               },
               child: Text('Add Transaction'),
               style: ElevatedButton.styleFrom(
